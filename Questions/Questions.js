25d953825a2d6e0ac0acf37294038b413915bdfb
Nyfii <frunch04@gmail.com>
2025-09-25 13:28:31 +0200

    Added README

--- Questions/Questions.js
+++ Questions/Questions.js
@@ -42,9 +42,9 @@ window.BOT_OR_NOT_DATA = [
     {
         "question": "Wofür steht die Abkürzung \"AI\"?",
         "answers": [
+            "Artificial Intelligence",
             "Algorithmic Interface",
             "Automatic Interaction",
-            "Artificial Intelligence",
             "Advanced Innovation"
         ],
         "explanation": "..."
@@ -58,6 +58,130 @@ window.BOT_OR_NOT_DATA = [
             "1995"
         ],
         "explanation": "..."
+    },
+    {
+        "question": "Welcher dieser Texte ist eine Originalzitat von Göthe?",
+        "answers": [
+            "A: \"Zwei Seelen wohnen, ach! In meiner Brust. Die eine will sich von der andern trennen!\"",
+            "B: \"In meiner Brust erheben sich zwei Stimmen, die eine drängt ins Licht, die andre will ins Schweigen sinken.\""
+        ],
+        "explanation": "..."
+    },
+    {
+        "question": "Welcher dieser Texte ist ein Originalzitat von Rilke?",
+        "answers": [
+            "A: \"Herr: es ist Zeit. Der Sommer war sehr groß. Leg deinen Schatten auf die Sonnenuhren, und auf den Fluren laß die Winde los.\"",
+            "B: \"Der Sommer sinkt, als hätte er geschlaffen, der Herbst erwacht und füllt die stillen Räume mit Schatten, die sich über Felder legen\""
+        ],
+        "explanation": "..."
+    },
+    {
+        "question": "Wenn eine KI im medizinschen Bereich eine falsche Diagnose stellt - wär trägt die Verantwortung?",
+        "answers": [
+            "Der Arzt oder Hersteller",
+            "Die KI selbst"
+        ],
+        "explanation": "Die Verantwortung liegt bei menschlichen Akteuren wie Ärzten oder Herstellern. Nicht bei der KI selbst."
+    },
+    {
+        "question": "Würdest du einer KI vertrauen, die über ein Gerichtsverfahren entscheidet?",
+        "answers": [
+            "Nein, Entscheidungen im Rechtssystem sollte von Menschen getroffen werden",
+            "Ja, KI ist neutral und immer fair"
+        ],
+        "explanation": "Rechtlichte Entscheidungen erfordern menschliches Urteilsvermögen, Ethik und Kontextbewusstsein - Eigenschaften, die KI nicht besitzt."
+    },
+    {
+        "question": "Welche Programmiersprache wird besonders häufig für KI-Entwicklung genutzt?",
+        "answers": [
+            "Python",
+            "Java",
+            "C++",
+            "PHP"
+        ],
+        "explanation": "Python ist eine sehr einfach zu verwendene Sprache, in der es viele Bibliotheken und Frameworks gibt. Performancekritische Komponenten sind meist in C++ geschrieben."
+    },
+    {
+        "question": "Welche KI-Anwendung hat das Potential, demokratische Wahlen zu beeinflussen?",
+        "answers": [
+            "Alle Antworten sind richtig",
+            "Deepfake-Videos",
+            "Empfehlungssysteme in sozialen Medien",
+            "Automatische Nachrichten-Generierung"
+        ],
+        "explanation": "Deepfakes, Empfehlungssysteme und automatische Nachrichten können jeweils das Wahlverhalten manipuliern."
+    },
+    {
+        "question": "Welches Land hat 2024 das weltweit erste umfassende KI-Gesetz (\"AI-Act\") verabschiedet?",
+        "answers": [
+            "Europäische Union",
+            "USA",
+            "China",
+            "Japan"
+        ],
+        "explanation": "Sie hat klare Regeln zur Sicherheit, Transparenz und Risikoeinstufung von KI-Systemen eingeführt."
+    },
+    {
+        "question": "Welches dieser Beispiele ist ein Deepfake?",
+        "answers": [
+            "Ein manipuliertes Video, in dem eine Person Worte sagt, die sie nie gesagt hat",
+            "Ein Roboter, der Auto fährt",
+            "Ein Computerspiel mit KI-Gegnern",
+            "Eine App, die deine Schritte zählt"
+        ],
+        "explanation": "..."
+    },
+    {
+        "question": "Wofür steht die Abkürzung \"AI\"?",
+        "answers": [
+            "Artificial Intelligence",
+            "Algorithmic Interface",
+            "Automatic Interaction",
+            "Advanced Innovation"
+        ],
+        "explanation": "..."
+    },
+    {
+        "question": "Wann wurde der Begriff \"Künstliche Intelligenz\" offiziell eingeführt?",
+        "answers": [
+            "1956",
+            "1936",
+            "1972",
+            "1995"
+        ],
+        "explanation": "..."
+    },
+    {
+        "question": "Welcher dieser Texte ist eine Originalzitat von Göthe?",
+        "answers": [
+            "A: \"Zwei Seelen wohnen, ach! In meiner Brust. Die eine will sich von der andern trennen!\"",
+            "B: \"In meiner Brust erheben sich zwei Stimmen, die eine drängt ins Licht, die andre will ins Schweigen sinken.\""
+        ],
+        "explanation": "..."
+    },
+    {
+        "question": "Welcher dieser Texte ist ein Originalzitat von Rilke?",
+        "answers": [
+            "A: \"Herr: es ist Zeit. Der Sommer war sehr groß. Leg deinen Schatten auf die Sonnenuhren, und auf den Fluren laß die Winde los.\"",
+            "B: \"Der Sommer sinkt, als hätte er geschlaffen, der Herbst erwacht und füllt die stillen Räume mit Schatten, die sich über Felder legen\""
+        ],
+        "explanation": "..."
+    },
+    {
+        "question": "Wenn eine KI im medizinschen Bereich eine falsche Diagnose stellt - wär trägt die Verantwortung?",
+        "answers": [
+            "Der Arzt oder Hersteller",
+            "Die KI selbst"
+        ],
+        "explanation": "Die Verantwortung liegt bei menschlichen Akteuren wie Ärzten oder Herstellern. Nicht bei der KI selbst."
+    },
+    {
+        "question": "Würdest du einer KI vertrauen, die über ein Gerichtsverfahren entscheidet?",
+        "answers": [
+            "Nein, Entscheidungen im Rechtssystem sollte von Menschen getroffen werden",
+            "Ja, KI ist neutral und immer fair"
+        ],
+        "explanation": "Rechtlichte Entscheidungen erfordern menschliches Urteilsvermögen, Ethik und Kontextbewusstsein - Eigenschaften, die KI nicht besitzt."
     },
     {
         "question": "What is A Dog?",
---
+++ README.md
@@ -0,0 +1,80 @@
+**Bot or Not** Quiz App is a fullscreen quit app used in workshops. It shows questions (text, images, audio and videos) and gives instant feedback to the responses. It doesn't track score and runs directly in your browser.
+
+## Adding Questions
+
+### File Hierarchy
+
+``` 
+BotOrNot/
+	- index.html
+	- admin.html
+	- converter.html
+	Questions/
+		- questions.js
+		- QuestionFile1.ybki
+		- QuestionFile2.ybki
+		...
+	Resources
+		- MediaFile1.*
+		- MediaFile2.*
+```
+
+All the questions should go in the Questions folder, while MediaFiles like images, videos, audios go in the Resources Folder.
+
+### Adding new Questions file
+
+```YAML
+# For Media write the FileName. The File needs to be in the correct location. Following Formats are supported:
+# Image: .png, .jpg, .webp
+# Video: .mp4
+# Audio: .mp3, .wav
+
+# Text + 4 Answers (for 2 Answers have 2 answer Options)
+Question: [QUESTION_TEXT_HERE]
+Answer: [CORRECT_ANSWER]
+Answer: [INCORRECT_ANSWER]
+Answer: [INCORRECT_ANSWER]
+Answer: [INCORRECT_ANSWER]
+Explanation: [EXPLANATION]
+Label: [LABEL]
+
+---
+
+# 2 Media + 2 Answers
+Media: [CORRECT_MEDIA]
+Media: [INCCORECT_MEDIA]
+Explanation: [EXPLANATION]
+Label: [LABEL]
+
+---
+
+# 1 Media + 2 Answers
+Media: [MEDIA]
+Answer: [CORRECT_ANSWER]
+Answer: [INCORRECT_ANSWER]
+Explanation: [EXPLANATION]
+Label: [LABEL]
+```
+
+Where:
+
+- `Question`: Is the written part of a Questions
+- `Answer`: Are the Answer possibilities
+- `Media`: Is the Name of a Video, Image or Audio file that should be played
+- `Explanation`: Explanation why the Response is correct or incorrect
+- `Label`: Is the Label / Category of the Question
+
+`---` is the Question Separator. Single Line Comments can be written with `#` (Only at the beginning of a line). The Questions should be either grouped, by who asked them (if something is unclear this makes it easy asking back), or Question Type.
+
+**Naming Conventions**
+
+Questions should just be named: `[NAME].ybki`. The Media should be labeled `[NAME]_FAKE_[CONTENT].*` or `[NAME]_REAL_[CONTENT].*`
+
+### Converting `.ybki` to the `Questions.js` File
+
+1. Open the `converter.html` in your browser
+2. Upload one or more `.ybki` files
+3. Click **Parse & Merge** (Check for errors)
+4. Download the JS
+5. Save the generated `Questions.js` in the `Questions/` folder
+6. Refresh the Quiz App.
