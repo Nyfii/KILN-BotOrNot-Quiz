e20287b2a1013f59c969dbbb0f6492dc5ecf1bc6
Nyfii <frunch04@gmail.com>
2025-09-29 15:49:18 +0200

    Reswitched Files

---
+++ Questions/Questions_Test.js
@@ -0,0 +1,45 @@
+window.BOT_OR_NOT_DATA = [
+    {
+        "question": "Test",
+        "media": [
+            "Mathias_FAKE_Bundestag.jpeg"
+        ],
+        "answers": [
+            "...",
+            "..."
+        ],
+        "explanation": "...",
+        "label": "..."
+    },
+    {
+        "question": "...",
+        "media": [
+            "Mathias_FAKE_Bundestag.jpeg",
+            "Mathias_FAKE_ZelenskyPutin.jpeg"
+        ],
+        "explanation": "...",
+        "label": "..."
+    },
+    {
+        "question": "...",
+        "answers": [
+            "...",
+            "...",
+            "...",
+            "..."
+        ],
+        "explanation": "...",
+        "label": "..."
+    },
+    {
+        "question": "...",
+        "answers": [
+            "...",
+            "...",
+            "...",
+            "..."
+        ],
+        "explanation": "...",
+        "label": "..."
+    }
+]
