50383c1b1de9ce8366098161c80581ff83d3ed41
Nyfii <frunch04@gmail.com>
2025-09-29 15:42:10 +0200

    Added Image Question Support

--- Questions/Questions.js
+++ Questions/Questions.js
@@ -1,5 +1,17 @@
 window.BOT_OR_NOT_DATA = [
     {
+        "question": "Test",
+        "media": [
+            "Mathias_FAKE_Bundestag.jpeg"
+        ],
+        "answers": [
+            "...",
+            "..."
+        ],
+        "explanation": "...",
+        "label": "..."
+    },
+    {
         "question": "...",
         "media": [
             "Mathias_FAKE_Bundestag.jpeg",
@@ -13,18 +25,6 @@ window.BOT_OR_NOT_DATA = [
         "answers": [
             "...",
             "...",
-            "...",
-            "..."
-        ],
-        "explanation": "...",
-        "label": "..."
-    },
-    {
-        "question": "...",
-        "media": [
-            "Mathias_FAKE_Bundestag.jpeg"
-        ],
-        "answers": [
             "...",
             "..."
         ],
