b734e8a39b8fd0b02c82e80fb86c7a3c3208b6c0
Nyfii <frunch04@gmail.com>
2025-10-01 08:11:01 +0200

    Added better QuestionTesting
    Instead of renaming the file, now the normal and the test-questions both
    get loaded. To use the testquestions use the `loadTestQuestions()`
    method through the console.

---
+++ Questions/Questions_Test.js
@@ -0,0 +1,45 @@
+window.BOT_OR_NOT_TEST = [
+    {
+        "question": "Test",
+        "media": [
+            "TEST_Video.mp4"
+        ],
+        "answers": [
+            "...",
+            "..."
+        ],
+        "explanation": "...",
+        "label": "..."
+    },
+    {
+        "question": "...",
+        "media": [
+            "Mathias_FAKE_Bundestag.jpeg",
+            "Mathias_FAKE_ZelenskyPutin.jpeg"
+        ],
+        "explanation": "...",
+        "label": "..."
+    },
+    {
+        "question": "...",
+        "answers": [
+            "...",
+            "...",
+            "...",
+            "..."
+        ],
+        "explanation": "...",
+        "label": "..."
+    },
+    {
+        "question": "...",
+        "answers": [
+            "...",
+            "...",
+            "...",
+            "..."
+        ],
+        "explanation": "...",
+        "label": "..."
+    }
+]
